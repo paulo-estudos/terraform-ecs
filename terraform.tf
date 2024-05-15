d958dac1cf0f4b073f460bfdaac0b77000158df3
Paulo Roberto PC <enjoei.paulo@hotmail.com>
2024-05-14 23:21:31 -0300

    criacao ecs cluster na aws

---
+++ terraform.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_ecs_cluster" "ecs_libertines" {
+  name = "ecs_libertines"
+}
+
+/* resource "aws_ecs_task_definition" "my_task" {
+  family                   = "my-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([{
+    name  = "my-container"
+    image = "my-repo/my-image:latest"
+    portMappings = [{
+      containerPort = 8080
+      hostPort      = 8080
+      protocol      = "tcp"
+    }]
+  }])
+}
+
+resource "aws_ecs_service" "my_service" {
+  name            = "my-service"
+  cluster         = aws_ecs_cluster.my_cluster.id
+  task_definition = aws_ecs_task_definition.my_task.arn
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets = ["subnet-abcde012", "subnet-bcde012a"]
+    assign_public_ip = true
+  }
+
+  desired_count = 1
+} */
