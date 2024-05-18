4c4b33bebeacc1b03041d52bbea64481e4baefaa
Paulo Roberto PC <enjoei.paulo@hotmail.com>
2024-05-18 14:17:19 -0300

    terraform ecs v1

--- terraform.tf
+++ terraform.tf
@@ -8,44 +8,302 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
   access_key = var.access_key
   secret_key = var.secret_key
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    name = "main"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 2)
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "internet_gateway"
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "subnet1_association" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "subnet2_association" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "ecs_instance_role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role",
+  ]
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "ecs_instance_profile"
+  role = aws_iam_role.ecs_instance_role.name
+}
+
+resource "aws_security_group" "ecs_security_group" {
+  name   = "ecs_security_group"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_ecs_cluster" "ecs_libertines" {
   name = "ecs_libertines"
 }
 
-/* resource "aws_ecs_task_definition" "my_task" {
-  family                   = "my-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
+resource "aws_ecs_service" "up_the_bracket" {
+  name            = "up-the-bracket"
+  cluster         = aws_ecs_cluster.ecs_libertines.id
+  task_definition = aws_ecs_task_definition.time_for_heroes.arn
+  launch_type     = "EC2"
+
+  desired_count = 1
+
+  deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent = 200
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.main.arn
+    container_name   = "aws_ecs_app_sample"
+    container_port   = 80
+  }
+}
+
+resource "aws_ecs_task_definition" "time_for_heroes" {
+  family                   = "time-for-heroes"
+  network_mode             = "bridge"
+  requires_compatibilities = ["EC2"]
   cpu                      = "256"
   memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
 
   container_definitions = jsonencode([{
-    name  = "my-container"
-    image = "my-repo/my-image:latest"
+    name  = "aws_ecs_app_sample"
+    image = "public.ecr.aws/ecs-sample-image/amazon-ecs-sample:latest"
+    
     portMappings = [{
-      containerPort = 8080
-      hostPort      = 8080
+      containerPort = 80
+      hostPort      = 80
       protocol      = "tcp"
+    }]
+    cpu       = 128
+    memory    = 256
+    essential = true
+
+    environment = [{
+      name  = "FFKEY"
+      value = "dec627fe-59a7-4680-900e-6cd4db5d9f09"
     }]
   }])
 }
 
-resource "aws_ecs_service" "my_service" {
-  name            = "my-service"
-  cluster         = aws_ecs_cluster.my_cluster.id
-  task_definition = aws_ecs_task_definition.my_task.arn
-  launch_type     = "FARGATE"
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecs-ecsTaskRole"
 
-  network_configuration {
-    subnets = ["subnet-abcde012", "subnet-bcde012a"]
-    assign_public_ip = true
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs_task_execution_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_policy" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_launch_template" "ecs_lt" {
+  name_prefix   = "ecs-template"
+  image_id      = "ami-057f57c2fcd14e5f4"  # ECS-optimized AMI
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.deployer.key_name
+
+  vpc_security_group_ids = [aws_security_group.ecs_security_group.id]
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ecs_instance_profile.name
   }
 
-  desired_count = 1
-} */
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size = 30
+      volume_type = "gp2"
+    }
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "ecs-instance"
+    }
+  }
+
+  user_data = filebase64("${path.module}/ecs.sh")
+}
+
+resource "aws_autoscaling_group" "ecs_asg" {
+  desired_capacity     = 1
+  max_size             = 1
+  min_size             = 1
+  vpc_zone_identifier  = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+  launch_template {
+    id      = aws_launch_template.ecs_lt.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "ecs-instance"
+    propagate_at_launch = true
+  }
+}
+
+# Create SSH key pair
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("${path.module}/paulinho_ssh.pub")
+}
+
+# Load Balancer (NLB)
+resource "aws_lb" "main" {
+  name               = "main-nlb"
+  internal           = false
+  load_balancer_type = "network"
+  subnets            = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+
+  security_groups = [aws_security_group.ecs_security_group.id]
+
+  enable_deletion_protection = false
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Target Group
+resource "aws_lb_target_group" "main" {
+  name        = "main-target-group"
+  port        = 80
+  protocol    = "TCP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "instance"
+
+  health_check {
+    port     = "traffic-port"
+    protocol = "TCP"
+  }
+}
+
+# Listener
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
